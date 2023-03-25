d67bfe2b12559165de653daaaacf06f66ab49c3f
Gareth Stoyle <stoyleg@gmail.com>
2023-03-25 15:35:48 +0000

    Made into a flask server, added html files with jinja code.

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from get_sports import get_fixtures
+from flask import Flask, render_template
 
-url = "https://www.tvsportguide.com"
-fixtures = get_fixtures(url)
-for fixture in fixtures:
-    print(fixture, fixtures[fixture], "\n\n")
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome'
+
+@app.route('/sports')
+def sports():
+    url = "https://www.tvsportguide.com"
+    fixtures = get_fixtures(url)
+    print(fixtures)
+    #tweetData.sort(key=lambda x: x['airline_sentiment_confidence'])
+    return render_template("sports.html", fixture_data=fixtures)
+
+
+app.run(host='0.0.0.0', port=81)
